7c440bbcf990c3d200c93693cf131827b3e7b00a
Bartlomiej Kozal <bkzl@me.com>
2017-02-23 22:25:39 +0100

    Indent long placeholders

--- components/FloatLabel.vue
+++ components/FloatLabel.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="vfl-has-label">
-    <div class="vfl-label">{{ placeholder }}</div>
+    <div class="vfl-label" :style="{ width }" @click="">
+      {{ placeholder }}
+    </div>
     <slot></slot>
   </div>
 </template>
@@ -10,12 +12,16 @@ export default {
   name: 'float-label',
   data () {
     return {
-      placeholder: ''
+      placeholder: '',
+      width: 'auto'
     }
   },
   mounted () {
     const el = this.$el.querySelector('input, textarea')
+    const rightPadding = 6
+
     this.placeholder = el.placeholder
+    this.width = `${el.clientWidth - rightPadding}px`
     el.placeholder = ''
   }
 }
@@ -30,7 +36,6 @@ export default {
   position: absolute;
   top: 0.25em;
   left: 0.3em;
-  width: 100%;
   overflow: hidden;
   font-family: sans-serif;
   font-size: 0.7em;
